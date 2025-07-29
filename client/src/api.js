3fc1f5edfa41df84960c52a938f8a280375d45de
sodhavanraj7 <sodhavanrajsinh95@gmail.com>
2025-07-29 15:35:46 +0530

    Connected frontend to Render backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
- 
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'https://taxi-booking-backend-muup.onrender.com',
+  baseURL: "https://taxi-booking-backend-muup.onrender.com", // ✅ correct
 });
 
 export default API;
+
 
